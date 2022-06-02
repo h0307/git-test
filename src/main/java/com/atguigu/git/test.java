1479f0c8710c0c27a48124d7968414cbb96b96ec
将离 <15938214274@163.com>
2022-06-02 09:12:08 +0800

    one

---
+++ src/main/java/com/atguigu/git/test.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author mo
+ * Create by 2022/6/2 9:07
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("git 1");
+    }
+}
