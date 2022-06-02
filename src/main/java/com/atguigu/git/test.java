057be426c25285310287958f7f9fdaa0bba24be1
将离 <15938214274@163.com>
2022-06-02 10:06:26 +0800

    three

--- src/main/java/com/atguigu/git/test.java
+++ src/main/java/com/atguigu/git/test.java
@@ -8,5 +8,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("git 1");
         System.out.println("git 2");
+        System.out.println("git 3");
     }
 }
